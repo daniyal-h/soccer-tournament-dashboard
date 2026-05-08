dd562356a972b81021a68a04db566db14670063d
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-08 15:15:52 -0500

    chore: add config as settings

---
+++ backend/app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    VERSION: str = "0.1.0"
+    DATABASE_URL: str
+    ENVIRONMENT: str = "development"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
