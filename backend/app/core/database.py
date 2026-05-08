3a97045a1552d63216e99830347bcf558a965c54
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-08 16:03:06 -0500

    refactor: use config settings to extract database url

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,14 +1,9 @@
-import os
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-load_dotenv()
+from app.core.config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
