aef240ae8c03163744c44de29d5f8ae592c6247d
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-09 18:05:37 -0500

    chore: uncomment implemented routers

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -4,10 +4,10 @@ from app.api.v1.routers import admin, health, matches, player_stats, search, sta
 
 api_router = APIRouter()
 
-# api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
+api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
 api_router.include_router(health.router, prefix="/health", tags=["health"])
 api_router.include_router(matches.router, prefix="/matches", tags=["matches"])
-# api_router.include_router(player_stats.router, prefix="/player_stats", tags=["player_stats"])
-# api_router.include_router(search.router, prefix="/search", tags=["search"])
+api_router.include_router(player_stats.router, prefix="/player_stats", tags=["player_stats"])
+api_router.include_router(search.router, prefix="/search", tags=["search"])
 api_router.include_router(standings.router, prefix="/standings", tags=["standings"])
-# api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
+api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
