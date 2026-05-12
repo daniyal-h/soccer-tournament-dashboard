84a7b0efda22176cb780fff4416075a793497389
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-12 01:22:57 -0500

    ui: display currently selected tournament

--- frontend/src/components/pages/Standings.tsx
+++ frontend/src/components/pages/Standings.tsx
@@ -1,10 +1,14 @@
+import { useTournament } from "@/context/TournamentContext";
+
 const Standings = () => {
+  const { selectedTournamentId } = useTournament();
+
   return (
     <section className="space-y-2">
       <h1 className="text-3xl font-bold tracking-tight">Standings</h1>
 
       <p className="text-muted-foreground">
-        View selected tournament's standings.
+        View standings for {selectedTournamentId}.
       </p>
     </section>
   );
