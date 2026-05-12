8e95cde1fc4f4c4e8920efcc474dd7646bca497a
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-12 00:39:18 -0500

    ui: augment placeholders with more stylized text

--- frontend/src/components/pages/Schedule.tsx
+++ frontend/src/components/pages/Schedule.tsx
@@ -1,7 +1,13 @@
 const Schedule = () => {
   return (
-    <div>Schedule</div>
-  )
-}
+    <section className="space-y-2">
+      <h1 className="text-3xl font-bold tracking-tight">Schedule</h1>
 
-export default Schedule
+      <p className="text-muted-foreground">
+        View upcoming and completed tournament matches.
+      </p>
+    </section>
+  );
+};
+
+export default Schedule;
--- frontend/src/components/pages/Standings.tsx
+++ frontend/src/components/pages/Standings.tsx
@@ -1,5 +1,13 @@
-const StandingsPage = () => {
-  return <div>StandingsPage</div>;
+const Standings = () => {
+  return (
+    <section className="space-y-2">
+      <h1 className="text-3xl font-bold tracking-tight">Standings</h1>
+
+      <p className="text-muted-foreground">
+        View selected tournament's standings.
+      </p>
+    </section>
+  );
 };
 
-export default StandingsPage;
+export default Standings;
