3cddc07289edb887038cda4d9e3162f0bac30ea5
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-12 01:46:11 -0500

    chore: run prettier formatter

--- frontend/src/components/pages/Schedule.tsx
+++ frontend/src/components/pages/Schedule.tsx
@@ -3,9 +3,7 @@ const Schedule = () => {
     <section className="space-y-2">
       <h1 className="text-3xl font-bold tracking-tight">Schedule</h1>
 
-      <p className="text-muted-foreground">
-        View upcoming and completed tournament matches.
-      </p>
+      <p className="text-muted-foreground">View upcoming and completed tournament matches.</p>
     </section>
   );
 };
