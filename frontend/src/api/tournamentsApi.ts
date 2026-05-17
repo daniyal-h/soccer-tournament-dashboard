37b439fcd71b0f714ca2a287151687e39f79fab1
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-17 00:32:59 -0500

    chore: correct import

--- frontend/src/api/tournamentsApi.ts
+++ frontend/src/api/tournamentsApi.ts
@@ -1,6 +1,5 @@
 import { apiGet } from './client';
-
-import { type Tournament } from '@/types/tournaments';
+import { type Tournament } from '@/types/tournament';
 
 export function getTournaments() {
   return apiGet<Tournament[]>('/api/v1/tournaments/');
