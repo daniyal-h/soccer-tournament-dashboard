cbd7b563e9550ee863577af289b1d79b2bdd3873
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-17 01:20:04 -0500

    fix: remove API versioning URL since env has them

--- frontend/src/api/tournamentsApi.ts
+++ frontend/src/api/tournamentsApi.ts
@@ -2,5 +2,5 @@ import { apiGet } from './client';
 import { type Tournament } from '@/types/tournament';
 
 export function getTournaments() {
-  return apiGet<Tournament[]>('/api/v1/tournaments/');
+  return apiGet<Tournament[]>('/tournaments/');
 }
