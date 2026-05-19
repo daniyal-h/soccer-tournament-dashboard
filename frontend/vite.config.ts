02ab267a71f0daa43eb82a838ea2229b2cdcae58
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-18 19:12:30 -0500

    chore: configure vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import path from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,5 +10,11 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
   },
 });
