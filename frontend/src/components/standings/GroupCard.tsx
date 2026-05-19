904d0562b2354af6267470b11f6dc7bb9ac1d6b1
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-19 02:44:04 -0500

    feat: implement group card to contain an accordion to display the group table of team summaries

---
+++ frontend/src/components/standings/GroupCard.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { GroupTable } from "./GroupTable";
+import { type Standing } from "@/constants/standings";
+
+interface GroupCardProps {
+  group: string;
+  rows: Standing[];
+}
+
+// a card contains an accordion with its group identifier to show a group table
+const GroupCard = ({ group, rows }: GroupCardProps) => {
+  return (
+    <Card className="w-full shadow-sm">
+      <CardContent>
+        <Accordion type="single" collapsible defaultValue="standings">
+          <AccordionItem value="standings">
+            <AccordionTrigger>Group {group}</AccordionTrigger>
+            <AccordionContent>
+              <GroupTable rows={rows} />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default GroupCard;
