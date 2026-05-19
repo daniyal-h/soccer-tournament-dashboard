389f0ebefb1e449fc58f833f31cf51efcc55a982
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-19 15:07:07 -0500

    refactor: make Team its own interface

--- frontend/src/types/standings.ts
+++ frontend/src/types/standings.ts
@@ -1,11 +1,8 @@
+import { type Team } from './team';
+
 // a Standing includes some details of its team
 export interface Standing {
-  team: {
-    id: number;
-    name: string;
-    short_name: string;
-    logo_url: string;
-  };
+  team: Team;
   position: number;
   matches_played: number;
   wins: number;
