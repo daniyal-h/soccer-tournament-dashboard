f507ef17d8a8b451df4fb1968db3c087360533d0
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-20 18:09:54 -0500

    chore: fix the return type to a list of tournament teams

--- backend/app/api/v1/repositories/tournament_teams.py
+++ backend/app/api/v1/repositories/tournament_teams.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from app.models.tournament_team import TournamentTeam
 
 
-def get_teams_in_tournament(db: Session, tournament_id: int):
+def get_teams_in_tournament(db: Session, tournament_id: int) -> list[TournamentTeam]:
     return db.query(TournamentTeam).where(TournamentTeam.tournament_id == tournament_id).all()
