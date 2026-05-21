4ab491379f499a013f21d4c8621a5ee28a691cd0
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-21 18:16:44 -0500

    chore: add constants to store all cache TTLs

---
+++ backend/app/constants/cache_ttl.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+
+STANDINGS_TTL = timedelta(minutes=1)
+STANDINGS_PRE_TOURNAMENT_TTL = timedelta(hours=24)
+MATCHES_TTL = timedelta(minutes=1)
+PLAYER_STATS_TTL = timedelta(minutes=5)
+TOURNAMENTS_TTL = timedelta(hours=24)
