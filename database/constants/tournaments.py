12acbc065e4b84ec540bf4241f717d7489191db1
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-22 18:04:59 -0500

    chore: reduce scope of supported tournaments to those which match World Cup format

--- database/constants/tournaments.py
+++ database/constants/tournaments.py
@@ -1,21 +1,12 @@
-# (local_db_id, external_api_id, season)
+from datetime import date
+
+# (local_db_id, external_api_id, season, end_date)
 SUPPORTED_TOURNAMENTS = [
-    (1, 1, "2022"),  # FIFA World Cup 2022
-    # (2, 1, "2026"),  # FIFA World Cup 2026
-    # (3, 8, "2023"),  # FIFA Women's World Cup 2023
-    (4, 4, "2024"),  # UEFA Euro 2024
-    # (5, 9, "2024"),  # Copa América 2024
-    # (6, 2, "2024"),  # UEFA Champions League 2024/25
-    # (7, 2, "2025"),  # UEFA Champions League 2025/26
-    (8, 39, "2024"),  # Premier League 2024/25
-    # (9, 39, "2025"),  # Premier League 2025/26
-    # (10, 140, "2024"),  # La Liga 2024/25
-    # (11, 140, "2025"),  # La Liga 2025/26
-    # (12, 78, "2024"),  # Bundesliga 2024/25
-    # (13, 78, "2025"),  # Bundesliga 2025/26
-    # (14, 135, "2024"),  # Serie A 2024/25
-    # (15, 135, "2025"),  # Serie A 2025/26
-    # (16, 61, "2024"),  # Ligue 1 2024/25
-    # (17, 61, "2025"),  # Ligue 1 2025/26
-    # (18, 6, "2025"),  # Africa Cup of Nations 2025
+    (1, 1, "2022", date(2022, 12, 18)),  # FIFA World Cup 2022
+    (2, 1, "2026", date(2026, 7, 19)),  # FIFA World Cup 2026
+    (3, 8, "2023", date(2023, 8, 20)),  # FIFA Women's World Cup 2023
+    (4, 4, "2024", date(2024, 7, 14)),  # UEFA Euro 2024
+    (5, 9, "2024", date(2024, 7, 14)),  # Copa America 2024
+    (100, 15, "2025", date(2025, 7, 13)), # Fifa Club World Cup 2025
+    (18, 6, "2025", date(2025, 12, 21)),  # Africa Cup of Nations 2025
 ]
