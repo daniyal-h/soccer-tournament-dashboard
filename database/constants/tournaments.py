017ba9eba1cf943c799998b16014fff61b8df601
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-22 17:43:30 -0500

    chore: test with more supported tournaments

--- database/constants/tournaments.py
+++ database/constants/tournaments.py
@@ -3,11 +3,11 @@ SUPPORTED_TOURNAMENTS = [
     (1, 1, "2022"),  # FIFA World Cup 2022
     # (2, 1, "2026"),  # FIFA World Cup 2026
     # (3, 8, "2023"),  # FIFA Women's World Cup 2023
-    # (4, 4, "2024"),  # UEFA Euro 2024
+    (4, 4, "2024"),  # UEFA Euro 2024
     # (5, 9, "2024"),  # Copa América 2024
     # (6, 2, "2024"),  # UEFA Champions League 2024/25
     # (7, 2, "2025"),  # UEFA Champions League 2025/26
-    # (8, 39, "2024"),  # Premier League 2024/25
+    (8, 39, "2024"),  # Premier League 2024/25
     # (9, 39, "2025"),  # Premier League 2025/26
     # (10, 140, "2024"),  # La Liga 2024/25
     # (11, 140, "2025"),  # La Liga 2025/26
