fec11e969f8e65ac17de56c5249cae817d496fa3
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-21 22:28:12 -0500

    chore: add a set of jobs refresh jobs can have as a name

---
+++ backend/app/constants/jobs.py
@@ -0,0 +1,5 @@
+# fixed set of refresh job names
+class JobName:
+    STANDINGS_REFRESH = "standings_refresh"
+    MATCHES_REFRESH = "matches_refresh"
+    PLAYER_STATS_REFRESH = "player_stats_refresh"
