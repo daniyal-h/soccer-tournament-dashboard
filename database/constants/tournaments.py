efacf4a8a94dec1559f8a2ca9ec17febf288c710
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-21 19:56:19 -0500

    chore: only support World Cup 2022 for testing

--- database/constants/tournaments.py
+++ database/constants/tournaments.py
@@ -1,21 +1,21 @@
 # (local_db_id, external_api_id, season)
 SUPPORTED_TOURNAMENTS = [
     (1, 1, "2022"),  # FIFA World Cup 2022
-    (2, 1, "2026"),  # FIFA World Cup 2026
-    (3, 8, "2023"),  # FIFA Women's World Cup 2023
-    (4, 4, "2024"),  # UEFA Euro 2024
-    (5, 9, "2024"),  # Copa América 2024
-    (6, 2, "2024"),  # UEFA Champions League 2024/25
-    (7, 2, "2025"),  # UEFA Champions League 2025/26
-    (8, 39, "2024"),  # Premier League 2024/25
-    (9, 39, "2025"),  # Premier League 2025/26
-    (10, 140, "2024"),  # La Liga 2024/25
-    (11, 140, "2025"),  # La Liga 2025/26
-    (12, 78, "2024"),  # Bundesliga 2024/25
-    (13, 78, "2025"),  # Bundesliga 2025/26
-    (14, 135, "2024"),  # Serie A 2024/25
-    (15, 135, "2025"),  # Serie A 2025/26
-    (16, 61, "2024"),  # Ligue 1 2024/25
-    (17, 61, "2025"),  # Ligue 1 2025/26
-    (18, 6, "2025"),  # Africa Cup of Nations 2025
+    # (2, 1, "2026"),  # FIFA World Cup 2026
+    # (3, 8, "2023"),  # FIFA Women's World Cup 2023
+    # (4, 4, "2024"),  # UEFA Euro 2024
+    # (5, 9, "2024"),  # Copa América 2024
+    # (6, 2, "2024"),  # UEFA Champions League 2024/25
+    # (7, 2, "2025"),  # UEFA Champions League 2025/26
+    # (8, 39, "2024"),  # Premier League 2024/25
+    # (9, 39, "2025"),  # Premier League 2025/26
+    # (10, 140, "2024"),  # La Liga 2024/25
+    # (11, 140, "2025"),  # La Liga 2025/26
+    # (12, 78, "2024"),  # Bundesliga 2024/25
+    # (13, 78, "2025"),  # Bundesliga 2025/26
+    # (14, 135, "2024"),  # Serie A 2024/25
+    # (15, 135, "2025"),  # Serie A 2025/26
+    # (16, 61, "2024"),  # Ligue 1 2024/25
+    # (17, 61, "2025"),  # Ligue 1 2025/26
+    # (18, 6, "2025"),  # Africa Cup of Nations 2025
 ]
