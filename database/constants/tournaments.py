07043c442950fadfad285e590bf5f102d39c0351
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-22 20:07:04 -0500

    chore: change supported tournaments

--- database/constants/tournaments.py
+++ database/constants/tournaments.py
@@ -2,11 +2,10 @@ from datetime import date
 
 # (local_db_id, external_api_id, season, end_date)
 SUPPORTED_TOURNAMENTS = [
-    (1, 1, "2022", date(2022, 12, 18)),  # FIFA World Cup 2022
-    (2, 1, "2026", date(2026, 7, 19)),  # FIFA World Cup 2026
-    (3, 8, "2023", date(2023, 8, 20)),  # FIFA Women's World Cup 2023
-    (4, 4, "2024", date(2024, 7, 14)),  # UEFA Euro 2024
-    (5, 9, "2024", date(2024, 7, 14)),  # Copa America 2024
-    (100, 15, "2025", date(2025, 7, 13)), # Fifa Club World Cup 2025
-    (18, 6, "2025", date(2025, 12, 21)),  # Africa Cup of Nations 2025
+    # (1, 1, "2026", date(2026, 7, 19)),  # FIFA World Cup 2026
+    (2, 1, "2022", date(2022, 12, 18)),  # FIFA World Cup 2022
+    (3, 4, "2024", date(2024, 7, 14)),  # UEFA Euro 2024
+    (4, 9, "2024", date(2024, 7, 14)),  # Copa America 2024
+    # (5, 6, "2025", date(2025, 12, 21)),  # Africa Cup of Nations 2025
+    # (6, 15, "2025", date(2025, 7, 13)), # Fifa Club World Cup 2025
 ]
--- database/scripts/seeds/static/tournaments.sql
+++ database/scripts/seeds/static/tournaments.sql
@@ -3,17 +3,14 @@ BEGIN TRANSACTION;
 INSERT INTO tournaments (external_api_id, name, season, logo_url, start_date, end_date)
 VALUES
   -- FIFA World Cup
-  (1, 'FIFA World Cup', '2022', 'https://media.api-sports.io/football/leagues/1.png', '2022-11-20', '2022-12-18'),
   (1, 'FIFA World Cup', '2026', 'https://media.api-sports.io/football/leagues/1.png', '2026-06-11', '2026-07-19'),
-
-  -- FIFA Women's World Cup
-  (8, 'FIFA Women''s World Cup', '2023', 'https://media.api-sports.io/football/leagues/8.png', '2023-07-20', '2023-08-20'),
+  (1, 'FIFA World Cup', '2022', 'https://media.api-sports.io/football/leagues/1.png', '2022-11-20', '2022-12-18'),
 
   -- UEFA Euro
   (4, 'UEFA Euro', '2024', 'https://media.api-sports.io/football/leagues/4.png', '2024-06-14', '2024-07-14'),
 
-  -- Copa América
-  (9, 'Copa América', '2024', 'https://media.api-sports.io/football/leagues/9.png', '2024-06-21', '2024-07-15'),
+  -- Copa America
+  (9, 'Copa America', '2024', 'https://media.api-sports.io/football/leagues/9.png', '2024-06-21', '2024-07-15'),
 
   -- Africa Cup of Nations
   (6, 'Africa Cup of Nations', '2025', 'https://media.api-sports.io/football/leagues/6.png', '2025-12-21', '2026-01-18'),
