b45e7c8d64f25d06fb5e5c1bcc8ddcb4ab9c278f
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-22 23:23:28 -0500

    chore: add TTL for finished tournaments and update  TTLs

--- backend/app/constants/cache_ttl.py
+++ backend/app/constants/cache_ttl.py
@@ -1,7 +1,8 @@
 from datetime import timedelta
 
 STANDINGS_TTL = timedelta(minutes=1)
-STANDINGS_PRE_TOURNAMENT_TTL = timedelta(hours=24)
+STANDINGS_PRE_TOURNAMENT_TTL = timedelta(hours=12)
+STANDINGS_FINISHED_TOURNAMENT_TTL = timedelta(hours=24)
 MATCHES_TTL = timedelta(minutes=1)
 PLAYER_STATS_TTL = timedelta(minutes=5)
 TOURNAMENTS_TTL = timedelta(hours=24)
