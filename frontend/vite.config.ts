f4c4c2d703b549a2e1826157aa960053768d8674
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-05-25 01:30:54 -0500

    chore: add excluded tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,5 +16,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.stryker-tmp/**'],
   },
 });
