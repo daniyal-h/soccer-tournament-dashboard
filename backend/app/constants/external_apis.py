4e355e4797106db2a98793abdda74353583818a3
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-01 18:45:16 -0500

    chore: add constants for api football endpoints

--- backend/app/constants/external_apis.py
+++ backend/app/constants/external_apis.py
@@ -1,1 +1,5 @@
 API_FOOTBALL_BASE_URL = "https://v3.football.api-sports.io"
+
+API_FOOTBALL_STANDINGS_ENDPOINT = "/standings"
+API_FOOTBALL_FIXTURES_ENDPOINT = "/fixtures"
+API_FOOTBALL_EVENTS_ENDPOINT = "/fixtures/events"
