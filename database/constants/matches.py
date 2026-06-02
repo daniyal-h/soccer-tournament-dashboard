c4b27f7cc7f5d898dfe275ec278d43e54efdb3ff
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-01 19:51:01 -0500

    fix: restore constants for generators

---
+++ database/constants/matches.py
@@ -0,0 +1,35 @@
+# codes based on Football-API fixture status can be collapsed into 5 types used in backend
+
+LIVE_STATUS_SHORT_CODES = {
+    "1H",
+    "HT",
+    "2H",
+    "ET",
+    "BT",
+    "P",
+    "SUSP",
+    "INT",
+    "LIVE",
+}
+
+FINISHED_STATUS_SHORT_CODES = {
+    "FT",
+    "AET",
+    "PEN",
+}
+
+SCHEDULED_STATUS_SHORT_CODES = {
+    "TBD",
+    "NS",
+}
+
+POSTPONED_STATUS_SHORT_CODES = {
+    "PST",
+}
+
+CANCELLED_STATUS_SHORT_CODES = {
+    "CANC",
+    "ABD",
+    "AWD",
+    "WO",
+}
