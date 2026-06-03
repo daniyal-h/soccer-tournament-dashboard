7a1875693753626dc59d241ccbf9387c9e63dffa
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-03 15:23:03 -0500

    feat: expose endpoint to get a desired match

--- backend/app/api/v1/routers/matches.py
+++ backend/app/api/v1/routers/matches.py
@@ -4,11 +4,24 @@ from fastapi import APIRouter, Depends, Path, Request
 from sqlalchemy.orm import Session
 
 from app.api.v1.services import match_events as match_events_service
+from app.api.v1.services import matches as matches_service
 from app.core.database import get_db
 from app.middleware.rate_limit import limiter
 from app.schemas.match_events import MatchEventResponse
+from app.schemas.matches import MatchesResponse
 
 router = APIRouter()
+
+
+@router.get("/{match_id}", response_model=MatchesResponse)
+@limiter.limit("60/minute")
+def get_match(
+    request: Request, db: Annotated[Session, Depends(get_db)], match_id: Annotated[int, Path(gt=0)]
+):
+    """
+    Return the specified match if it exists.
+    """
+    return matches_service.get_match(db, match_id)
 
 
 @router.get("/{match_id}/events", response_model=list[MatchEventResponse])
