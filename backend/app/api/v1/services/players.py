2ce99d11a7625489b94f1e7761667e568e375a10
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-06 18:38:15 -0500

    chore: accept null id to return null

--- backend/app/api/v1/services/players.py
+++ backend/app/api/v1/services/players.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.api.v1.repositories import players as players_repo
 
 
-def get_optional_player_id_from_external_id(db: Session, external_api_id: int) -> int | None:
-    return players_repo.get_player_from_external_id(db, external_api_id).id
+def get_optional_player_id_from_external_id(db: Session, external_api_id: int | None) -> int | None:
+    if external_api_id is None:
+        return None
 
+    return players_repo.get_player_from_external_id(db, external_api_id).id
