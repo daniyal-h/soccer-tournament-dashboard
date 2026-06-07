4391fece61fe5bb9615b6440cdb3eaf73444881b
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-07 16:08:54 -0500

    chore: add tanstack query hook

---
+++ frontend/src/hooks/useApiQuery.ts
@@ -0,0 +1,52 @@
+import {
+  type QueryKey,
+  useQuery,
+  type UseQueryOptions,
+  type UseQueryResult,
+} from '@tanstack/react-query';
+
+import { getApiErrorState } from '@/utils/errors/apiErrorHelper';
+
+interface ApiErrorMessages {
+  notFound: string;
+  generic: string;
+}
+
+interface UseApiQueryOptions<TQueryFnData, TData> extends Omit<
+  UseQueryOptions<TQueryFnData, Error, TData, QueryKey>,
+  'queryKey' | 'queryFn'
+> {
+  queryKey: QueryKey;
+  queryFn: () => Promise<TQueryFnData>;
+  errorMessages: ApiErrorMessages;
+}
+
+type ApiQueryResult<TData> = UseQueryResult<TData, Error> & {
+  displayError: Error | null;
+  canRetry: boolean;
+  isInitialLoading: boolean;
+  isRefreshing: boolean;
+};
+
+export function useApiQuery<TQueryFnData, TData = TQueryFnData>({
+  queryKey,
+  queryFn,
+  errorMessages,
+  ...options
+}: UseApiQueryOptions<TQueryFnData, TData>): ApiQueryResult<TData> {
+  const query = useQuery<TQueryFnData, Error, TData, QueryKey>({
+    queryKey,
+    queryFn,
+    ...options,
+  });
+
+  const errorState = query.error ? getApiErrorState(query.error, errorMessages) : null;
+
+  return {
+    ...query,
+    displayError: errorState ? new Error(errorState.message) : null,
+    canRetry: errorState?.canRetry ?? false,
+    isInitialLoading: query.isPending && !query.data,
+    isRefreshing: query.isFetching && !!query.data,
+  };
+}
