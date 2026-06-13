6ea68f971b9dc15f578c8cb8ef593fac63e30f7d
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-13 01:10:51 -0500

    feat: implement dynamic grid of team cards

---
+++ frontend/src/components/tournamentTeams/TeamCardGrid.tsx
@@ -0,0 +1,22 @@
+import type { TournamentTeam } from '@/types/tournamentTeam';
+
+import TeamCard from './TeamCard';
+
+interface TeamCardGridProps {
+  teams: TournamentTeam[];
+}
+
+/** A grid of 1-2 cards per row depending on screen size */
+const TeamCardGrid = ({ teams }: TeamCardGridProps) => {
+  return (
+    <div className="min-h-screen p-6">
+      <div className="grid grid-cols-1 gap-4 min-[1200px]:grid-cols-2">
+        {teams.map((tournamentTeam) => (
+          <TeamCard key={tournamentTeam.team.id} tournamentTeam={tournamentTeam} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TeamCardGrid;
