7c3e65713af8776d9b590afd16ec59c199e20d16
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-20 22:08:42 -0500

    chore: add stable team id

--- backend/load-tests/constants.js
+++ backend/load-tests/constants.js
@@ -1,6 +1,7 @@
 export const BASE_URL = __ENV.BASE_URL || 'http://localhost:8000';
 
-export const TOURNAMENT_ID = 1;
+export const TOURNAMENT_ID = 2;
 
 // Must exist in seeded test database
 export const MATCH_ID = 169;
+export const TEAM_ID = 20;
