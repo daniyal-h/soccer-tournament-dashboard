21e406c7a3238106e4f206bb459c516496689338
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-22 00:09:49 -0500

    refactor: define interfaces to be used across files

---
+++ frontend/src/types/playerLeaderboard.ts
@@ -0,0 +1,19 @@
+import type { PlayerSimpleSummary } from './player';
+import type { TeamSummary } from './team';
+
+export type LeadershipType = 'goals' | 'assists' | 'yellow_cards';
+
+export interface PlayerLeaderboardOptions {
+  tournament_id: number;
+  category: LeadershipType;
+}
+
+export interface PlayerLeaderboard {
+  rank: number;
+  value: number;
+  player: PlayerSimpleSummary;
+  team: TeamSummary;
+  appearances: number | null;
+  minutes_played: number | null;
+  rating: number | null;
+}
