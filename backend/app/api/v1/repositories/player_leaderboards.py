cf7590f5b858dd5b4229a85bd2a7df44ab72dc37
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-21 23:03:01 -0500

    chore: add db query for leaderboard for specified tournament and category

---
+++ backend/app/api/v1/repositories/player_leaderboards.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+
+from app.models.enums import LeaderboardType
+from app.models.player_leaderboards import PlayerLeaderboard
+from app.schemas.player_leaderboards import RankedPlayer
+
+
+def get_tournament_leaderboard_by_category(
+    db: Session, tournament_id: int, category: LeaderboardType
+) -> list[RankedPlayer]:
+    return (
+        db.query(PlayerLeaderboard)
+        .where(
+            PlayerLeaderboard.tournament_id == tournament_id, PlayerLeaderboard.category == category
+        )
+        .order_by(PlayerLeaderboard.rank.asc())
+        .limit(20)
+        .all()
+    )
