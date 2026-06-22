766c5e00d15005271229d491da441e1aecdc419c
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-21 23:54:31 -0500

    refactor: separate simple summary from full form to reuse

--- frontend/src/types/player.ts
+++ frontend/src/types/player.ts
@@ -1,9 +1,12 @@
-export interface PlayerSummary {
+export interface PlayerSimpleSummary {
   id: number;
   display_name: string;
+  photo_url: string | null;
+}
+
+export interface PlayerSummary extends PlayerSimpleSummary {
   first_name: string | null;
   last_name: string | null;
-  photo_url: string | null;
   nationality: string | null;
   date_of_birth: string | null;
   height: number | null;
