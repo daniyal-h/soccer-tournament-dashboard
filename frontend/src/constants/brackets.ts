8809cea8ff870ba6acc619e42cd7c59d79bb82ee
Daniyal Hasnain <daniyal-hasnain@outlook.com>
2026-06-23 21:31:21 -0500

    chore: define an empty bracket response to safely coalesce to

---
+++ frontend/src/constants/brackets.ts
@@ -0,0 +1,10 @@
+import type { BracketResponse } from '@/types/bracket';
+
+export const EMPTY_BRACKET: BracketResponse = {
+  round_of_32: [],
+  round_of_16: [],
+  quarter_final: [],
+  semi_final: [],
+  third_place: [],
+  final: [],
+};
